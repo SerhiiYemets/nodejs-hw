efcfb19f6e666190c9a2ff6d399302218657f2b9
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-04 11:36:25 +0100

    init express branch

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/notes',(req, res) => {
+  res.status(200).json({
+    message: "Retrieved all notes"
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({
+    message: "Retrieved note with ID: id_param"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
