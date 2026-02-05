561853e631b4acca9cf18ac26b236a9a498b3743
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-05 11:01:34 +0100

    server file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from "cors";
+import pino from "pino-http";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes',(req, res) => {
   res.status(200).json({
@@ -14,8 +34,24 @@ app.get('/notes/:noteId',(req, res) => {
   const { noteId } = req.params;
 
   res.status(200).json({
-    id_param: noteId,
     message: `Retrieved note with ID: ${noteId}`
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found"
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error("Error", err.message);
+  res.status(500).json({
+    message: err.message
   });
 });
 
