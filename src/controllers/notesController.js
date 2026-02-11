7096bdd3f270dfdeff43bfbee04ff901f932c01e
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-11 16:16:55 +0100

    MongoDB CRUD for notes

---
+++ src/controllers/notesController.js
@@ -0,0 +1,58 @@
+import { Note } from '../models/note.js';
+import createHttpError from 'http-errors';
+
+// Отримати список усіх нотатків
+export const getAllNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+// Отримати одну нотатку за id
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+// створення нотатки
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+//видалення нотатки
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, "Note not found");
+  }
+
+  res.status(200).json(note);
+};
+
+//оновлення нотатки
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findByIdAndUpdate(
+    { _id: noteId },
+    req.body,
+    { new: true },
+  );
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routers/notesRoutes.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3030;
 
-app.get('/notes',(req, res) => {
-  res.status(200).json({
-    message: "Retrieved all notes"
-  });
-});
+app.use(logger);
+app.use(express.json());
+app.use(cors());
 
-app.get('/notes/:noteId', (req, res) => {
-  res.status(200).json({
-    message: "Retrieved note with ID: id_param"
-  });
-});
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
