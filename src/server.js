eef9d8717972aa6f99c3662d834b67b94fc32724
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-16 19:54:55 +0100

    validations file

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import { errors } from "celebrate";
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
+
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
@@ -19,7 +21,7 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
-
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
