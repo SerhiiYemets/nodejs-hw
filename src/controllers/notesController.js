71d686f449ad311a71d83010ee0729fa67a5aedf
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-17 19:26:06 +0100

    All file

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,8 +3,34 @@ import createHttpError from 'http-errors';
 
 // Отримати список усіх нотатків
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page = 1, perPage = 10, tag, search } = req.query;
+
+  const skip = (page - 1) * perPage;
+
+  const filter = {};
+
+  if (tag) {
+    filter.tag = tag;
+  }
+
+  if (search) {
+    filter.$text = { $search: search };
+  }
+
+  const [totalNotes, notes] = await Promise.all([
+    Note.countDocuments(filter),
+    Note.find(filter).skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes
+  });
 };
 
 // Отримати одну нотатку за id
