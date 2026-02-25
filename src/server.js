292686753b10c94b5b99e1702e77524bacb317db
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-25 11:22:59 +0100

    server.js file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-import { errors } from "celebrate";
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 
@@ -10,14 +11,17 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger);
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
@@ -26,4 +30,6 @@ app.use(errorHandler);
 
 await connectMongoDB();
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
