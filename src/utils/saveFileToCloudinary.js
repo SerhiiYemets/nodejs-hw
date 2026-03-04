315502b781a3884fd10bc27849a76a69669ef1b0
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-03-04 04:01:45 +0100

    saveFileCloudinary file

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,15 +1,14 @@
-import { Readable } from 'node:stream';
 import { v2 as cloudinary } from 'cloudinary';
+import { Readable } from 'node:stream';
 
 cloudinary.config({
-  secure: true,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const saveFileToCloudinary = (buffer) => {
-  return new Promise((resolve, reject) => {
+export async function saveFileToCloudinary(buffer) {
+  const result = await new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: 'notes-app/avatars',
@@ -28,4 +27,6 @@ export const saveFileToCloudinary = (buffer) => {
 
     Readable.from(buffer).pipe(uploadStream);
   });
-};
+
+  return result;
+}
